ba0c1c3b62218d593469fa0fe0e37622f5725e7d
KunPeng <chinaxiangkun@gmail.com>
2026-05-31 00:57:36 +0800

    2026-07-08

--- data/spy/spy-allow-bode.js
+++ data/spy/spy-allow-bode.js
@@ -23,4 +23,25 @@
 
 var spy_allow_bodeRecords = [
   /* 暂无记录 */
+
+  /* 示例记录 */
+  {
+    playerId:   "Salat",
+    clearTime:  "0:32.850",
+    uploadTime: "2026-01-22",
+    videoUrls:  ["https://www.youtube.com/watch?v=Rorx4ZFNejA"],
+  },
+  {
+    playerId:   "L1ndell",
+    clearTime:  "0:33.167",
+    uploadTime: "2026-01-10",
+    videoUrls:  ["https://www.youtube.com/watch?v=BHHA_3yNIFk"],
+  },  
+  {
+    playerId:   "Mortar",
+    clearTime:  "0:37.000",
+    uploadTime: "2024-03-17",
+    videoUrls:  ["https://www.youtube.com/watch?v=86KTQS_3Htg"],
+  },
+
 ];
