7202b56644ae7d90ab3643c887d5716cb74aee2b
KunPeng <chinaxiangkun@gmail.com>
2026-05-31 00:29:08 +0800

    虚空歼灭+1999坦克 榜单更新

--- data/exterminate/exterminate-normal-oxomoco.js
+++ data/exterminate/exterminate-normal-oxomoco.js
@@ -23,4 +23,35 @@
 
 var exterminate_normal_oxomocoRecords = [
   /* 暂无记录 */
+  {
+       playerId:   "Jagdtor",
+       clearTime:  "0:12.550",
+       uploadTime: "2026-04-27",
+       videoUrls:  ["https://www.youtube.com/watch?v=AeFZigvQNS8&t=30s"],
+  },
+  {
+       playerId:   "yukkpo",
+       clearTime:  "0:14.933",
+       uploadTime: "2026-05-23",
+       videoUrls:  ["https://www.bilibili.com/video/BV1d4G56PEFM"],
+  },
+  {
+       playerId:   "nykroze",
+       clearTime:  "0:17.767",
+       uploadTime: "2026-05-01",
+       videoUrls:  ["hhttps://www.youtube.com/watch?v=UGk9O04zalU"],
+  },
+  {
+       playerId:   "Salat",
+       clearTime:  "0:18.383",
+       uploadTime: "2026-04-09",
+       videoUrls:  ["https://www.youtube.com/watch?v=7NbovDEZSo8"],
+  },
+  {
+       playerId:   "_Phobos",
+       clearTime:  "0:19.667",
+       uploadTime: "2026-03-29",
+       videoUrls:  ["https://www.youtube.com/watch?v=zZzelKHmdKM"],
+  },
+
 ];
