b3823ee2faa2b4c865bfefd84d79795245159f72
KunPeng <chinaxiangkun@gmail.com>
2026-05-31 05:00:01 +0800

    2026-07-08

--- data/capture/capture-mantle.js
+++ data/capture/capture-mantle.js
@@ -22,5 +22,23 @@
  * ════════════════════════════════════════════════════════════ */
 
 var capture_mantleRecords = [
-  /* 暂无记录 */
+
+  {
+  playerId:   "Salat",
+  clearTime:  "0:12.966",
+  uploadTime: "2025-10-27",
+  videoUrls:  ["https://www.youtube.com/watch?v=ebYdIqf7iTg"],
+},
+{
+  playerId:   "Mortar",
+  clearTime:  "0:13.933",
+  uploadTime: "2022-05-30",
+  videoUrls:  ["https://www.youtube.com/watch?v=aXJW0KLRqK0"],
+},
+{
+  playerId:   "OgvDams",
+  clearTime:  "0:14.707",
+  uploadTime: "2024-01-07",
+  videoUrls:  ["https://www.youtube.com/watch?v=5zjxGEhNJmQ"],
+},
 ];
