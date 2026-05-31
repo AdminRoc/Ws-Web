822f9df4b0877f94840f1dae31c897128c8bff58
KunPeng <chinaxiangkun@gmail.com>
2026-05-31 18:28:16 +0800

    2026-07-08

--- data/rescue/rescue-martialis.js
+++ data/rescue/rescue-martialis.js
@@ -22,5 +22,34 @@
  * ════════════════════════════════════════════════════════════ */
 
 var rescue_martialisRecords = [
-  /* 暂无记录 */
+  {
+    playerId:   "Salat",
+    clearTime:  "0:19.266",
+    uploadTime: "2026-01-11",
+    videoUrls:  ["https://www.youtube.com/watch?v=mmjNEEn2tjs"],
+  },
+  {
+    playerId:   "PrimedDarkeN",
+    clearTime:  "0:25.200",
+    uploadTime: "2022-06-26",
+    videoUrls:  ["https://www.youtube.com/watch?v=IZdrpT-F2fY"],
+  },
+  {
+    playerId:   "L1ndell",
+    clearTime:  "0:25.267",
+    uploadTime: "2022-11-11",
+    videoUrls:  ["https://www.youtube.com/watch?v=HMRo1zOrO3w"],
+  },
+  {
+    playerId:   "RoniPrime",
+    clearTime:  "0:25.900",
+    uploadTime: "2025-12-21",
+    videoUrls:  ["https://www.youtube.com/watch?v=-D8EbquA9tY"],
+  },
+  {
+    playerId:   "Febi",
+    clearTime:  "0:30.000",
+    uploadTime: "2022-01-08",
+    videoUrls:  ["https://www.youtube.com/watch?v=wz56OH2c1a8"],
+  },
 ];
