fdf95f9735c683244aca5c9966a35c3cabd74762
KunPeng <96916320+AdminRoc@users.noreply.github.com>
2026-06-21 11:53:08 +0800

    更新：仲裁页面

--- data/arbitration/arbitration-hourly-vitus-disruption.js
+++ data/arbitration/arbitration-hourly-vitus-disruption.js
@@ -27,5 +27,5 @@ var arbitration_hourly_vitus_disruptionRecords = [
  * Notice_cn : 默认显示的中文提醒（留空则不显示横幅）
  * Notice_en : 鼠标停留时显示的英文提醒（可留空）
  * ──────────────────────────────────────────────────────── */
-var arbitration_hourly_vitus_disruptionNotice_cn = "  单小时的生息精华数量，将把当次任务结算时显示的生息精华数量，按任务的实际时长进行换算。此处的“任务实际时长”按“首尾帧”的方式计算，具体计算方式请参考“提交规则”内的“通用规则”文档。";
-var arbitration_hourly_vitus_disruptionNotice_en = "  Vitus Essence per hour converts the Vitus Essence amount shown on the mission's end-of-mission screen according to the mission's actual duration. Here, the \"actual mission duration\" is measured using the first-and-last-frame method; for the exact calculation, please refer to the \"General Rules\" document under \"Submission Rules\".";
+var arbitration_hourly_vitus_disruptionNotice_cn = "  换算生息精华速率时，将把当次任务结算时显示的生息精华数量，按任务的实际时长进行换算。此处的“任务实际时长”按“首尾帧”的方式计算，具体计算方式请参考“提交规则”内的“通用规则”文档。此外，参与本榜单的任务记录的“任务实际时长”需要大于等于一小时。其他上传成绩要求，请查看“提交规则”内的“通用规则”文档。";
+var arbitration_hourly_vitus_disruptionNotice_en = "  When converting the Vitus Essence rate, the Vitus Essence amount shown on the mission's end-of-mission screen is converted according to the mission's actual duration. Here, the \"actual mission duration\" is measured using the first-and-last-frame method; for the exact calculation, please refer to the \"General Rules\" document under \"Submission Rules\". In addition, the \"actual mission duration\" of any run submitted to this leaderboard must be at least one hour. For other submission requirements, please refer to the \"General Rules\" document under \"Submission Rules\".";
