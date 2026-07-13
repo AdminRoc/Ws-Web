4b9ceecc200ebe9066354875e4e1bc2b3df66376
KunPeng <96916320+AdminRoc@users.noreply.github.com>
2026-07-14 04:44:13 +0800

    2026-07-14

--- data/disruption/disruption-kappa-solo.js
+++ data/disruption/disruption-kappa-solo.js
@@ -14,7 +14,7 @@
  */
 var disruptionKappaSoloRecords = [
   {
-    clearTime:  "52:16.000 [09Bug]",
+    clearTime:  "52:16.000",
     playerId:   "NamelessDeity",
     uploadTime: "2026-07-12",
     videoUrls:     ["https://www.youtube.com/watch?v=HhBmXLhpf9c"]
